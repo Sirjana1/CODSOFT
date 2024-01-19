297cacd41bcd05e056cae77b189aff4c48f98581
sirjana <email@.com>
2024-01-19 22:01:10 +0545

    calculator added

--- calculator.py
+++ calculator.py
@@ -18,7 +18,7 @@ class CalculatorApp:
             ('1', 3, 0), ('2', 3, 1), ('3', 3, 2), ('-', 3, 3), 
             ('0', 4, 0), ('.', 4, 1), ('=', 4, 2), ('+', 4, 3), 
         ]
-
+        # This is Sirjana practice code
         # Create and place buttons
         for (text, row, column) in buttons:
             button = tk.Button(master, text=text, command=lambda t=text: self.on_button_click(t))

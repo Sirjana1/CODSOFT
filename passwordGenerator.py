2fdc0f36ef914698d87e1c86bd0fca79690cf45c
sirjana <email@.com>
2024-01-21 10:50:31 +0545

    PasswordGenerator added

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -14,17 +14,14 @@ class PasswordGeneratorGUI:
         self.create_widgets()
 
     def create_widgets(self):
-        # Label and Entry for Password Length
         length_label = ttk.Label(self.root, text="Password Length:")
         length_entry = ttk.Entry(self.root, textvariable=self.length_var, width=5)
         length_label.grid(row=0, column=0, sticky="w")
         length_entry.grid(row=0, column=1, sticky="w")
 
-        # Button to generate password
         generate_button = ttk.Button(self.root, text="Generate Password", command=self.generate_password)
         generate_button.grid(row=1, column=0, columnspan=2, pady=10)
 
-        # Entry to display the generated password
         password_entry = ttk.Entry(self.root, textvariable=self.generated_password_var, state="readonly", width=30)
         password_entry.grid(row=2, column=0, columnspan=2, pady=10)
 
